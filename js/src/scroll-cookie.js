17e2eff06bca76f78adb2ea4eb370ec1d9163426
Melkor <pengiridis@icloud.com>
2018-10-07 14:22:20 +0800

    Site updated: 2018-10-07 14:22:19

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 24 2018 20:50:20 GMT+0800 (CST)
+// build time:Sun Oct 07 2018 14:22:19 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
